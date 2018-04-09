ee33b0f01378cffb0aef8d5b6f3c4b4bbc32fab4
leamsigc <leamsigc@hotmail.com>
2018-04-09 10:07:51 -0400

    add artials and create a engine view for node app

--- Demo3/index.js
+++ Demo3/index.js
@@ -4,18 +4,19 @@ const app = express();
 
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'))
-
+app.set('view engine','ejs');
 //home route 
 app.get('/', (req,res) => {
-    res.render('home.ejs');
+    res.render('home');
 });
 
 //fall in love route
 app.get('/love/:name',(req,res) => {
     let name = req.params.name;
     
-    res.render('love.ejs',{name:name});
+    res.render('love',{name:name});
 })
+
 
 //get posts
 app.get('/posts', (req,res) => {
@@ -38,7 +39,7 @@ app.get('/posts', (req,res) => {
         }
     ];
 
-    res.render('articles.ejs',{ articles:posts});
+    res.render('articles',{ articles:posts});
 })
 //app listen port
 app.listen(8080,() => console.log('app listen on port 8080'));
