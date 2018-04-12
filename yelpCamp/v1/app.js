143d8b7e46b040526c172775fadbb6b8c08f25a1
leamsigc <leamsigc@hotmail.com>
2018-04-12 16:23:49 -0400

    create the add new camground route

--- yelpCamp/v1/app.js
+++ yelpCamp/v1/app.js
@@ -16,26 +16,37 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 //camp ground 
-app.get('/campground', (req, res) => {
-    const campGroundArr = [{
-        name: 'salmon creak',
-        image: 'https://source.unsplash.com/VowIFDxogG4'
-    },
-    {
-        name: 'Mountain view',
-        image: 'https://source.unsplash.com/lpjb_UMOyx8'
-    },
-    {
-        name: 'San juan view',
-        image: 'https://source.unsplash.com/wdX9VkE_CnM'
-    }
+const campGroundArr = [{
+    name: 'salmon creak',
+    image: 'https://source.unsplash.com/VowIFDxogG4'
+},
+{
+    name: 'Mountain view',
+    image: 'https://source.unsplash.com/lpjb_UMOyx8'
+},
+{
+    name: 'San juan view',
+    image: 'https://source.unsplash.com/wdX9VkE_CnM'
+}
 ];
+app.get('/campground', (req, res) => {
     res.render('campground',{ campGroundArr : campGroundArr});
 });
 app.post('/campground',(req,res) => {
     //get form data
+    let formData= {
+        name: req.body.name,
+        image:req.body.url
+    };
+    campGroundArr.push(formData);
+    // console.log(formData);
     //redirect to 
     res.redirect('/campground');
 });
+
+//form for the new campground
+app.get('/campground/new',(req,res)=>{
+    res.render('form');
+})
 //listen port 
 app.listen(8080, () => console.log('App listening in port:3000'));
