5d360d47a2c32f3986c8326056f287582b0e6724
leamsigc <leamsigc@hotmail.com>
2018-05-22 12:55:09 -0400

    create comment api and redirect to the view page

--- yelpCamp/refactorv1/app.js
+++ yelpCamp/refactorv1/app.js
@@ -4,25 +4,29 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
     Camp = require('./models/campgrounds'),
+    Comments = require('./models/comments'),
     seedDB = require('./seeds');
 
 const app = express();
 
-seedDB();
+//seedDB();
 
 mongoose.connect('mongodb://localhost/yelp_camp_2');
 
-app.set('view engine', 'pug')
-    .use(bodyParser.urlencoded({
-        extended: true
-    }))
+app
+    .set('view engine', 'pug')
+    .use(
+        bodyParser.urlencoded({
+            extended: true
+        })
+    )
     .use(bodyParser.json())
     .use(express.static(__dirname + '/public'));
 
 let port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('campgrounds/home');
 });
 
 app.get('/campground', (req, res) => {
@@ -31,7 +35,7 @@ app.get('/campground', (req, res) => {
         if (err) {
             return console.log(err);
         }
-        res.render('campground', {
+        res.render('campgrounds/campground', {
             campGroundArr: allCampgrounds
         });
     });
@@ -54,10 +58,9 @@ app.post('/campground', (req, res) => {
 });
 
 app.get('/campground/new', (req, res) => {
-    res.render('form');
+    res.render('campgrounds/form');
 });
 
-/*SHOW ROUTER */
 app.get('/campground/:id', (req, res) => {
     let id = req.params.id;
 
@@ -68,10 +71,43 @@ app.get('/campground/:id', (req, res) => {
                 console.log(err);
             }
             console.log(foundCamp);
-            res.render('show', {
+            res.render('campgrounds/show', {
                 camp: foundCamp
             });
         });
+});
+
+app.get('/campground/:id/comments/new', (req, res) => {
+    let id = req.params.id;
+    console.log(id);
+
+    res.render('comments/new', {
+        id: id
+    });
+});
+
+app.post('/campground/:id/comments', (req, res) => {
+    let commentData = {
+        author: req.body.author,
+        text: req.body.text
+    };
+    let id = req.params.id;
+
+    Camp.findById(id, (err, foundCampground) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/campground');
+        }
+        Comments.create(commentData, (err, createdComment) => {
+            if (err) {
+                console.log(err);
+            }
+
+            foundCampground.comments.push(createdComment);
+            foundCampground.save();
+            res.redirect('/campground/' + id);
+        });
+    });
 });
 
 app.listen(port, () => console.log(`-App listen in port: ${port}`));
