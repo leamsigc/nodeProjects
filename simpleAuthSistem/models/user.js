4eab494d408e1bd3ee0121c613d4ddc99ae3b798
leamsigc <leamsigc@hotmail.com>
2018-05-25 16:07:12 -0400

    implement passport js and local strategy

--- simpleAuthSistem/models/user.js
+++ simpleAuthSistem/models/user.js
@@ -28,3 +28,23 @@ module.exports.createUser = (newUser, callback) => {
         });
     });
 };
+module.exports.getUserByUsername = (username, callback) => {
+    let query = {
+        username: username
+    };
+
+    User.findOne(query, callback);
+};
+
+module.exports.comparePassword = (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) {
+            throw err;
+        }
+        callback(null, isMatch);
+    });
+};
+
+module.exports.getUserById = (userId, callback) => {
+    User.findById(userId, callback);
+};
--- simpleAuthSistem/routes/users.js
+++ simpleAuthSistem/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express'),
     router = express.Router(),
     User = require('../models/user'),
     passport = require('passport'),
-    localStrategy = require('passport').Strategy;
+    localStrategy = require('passport-local').Strategy;
 
 //GET register
 router.get('/register', (req, res) => {
@@ -63,6 +63,41 @@ router.post('/register', (req, res) => {
 
 });
 
+
+passport.use(new localStrategy((username, password, done) => {
+    User.getUserByUsername(username, (err, user) => {
+        if (err) {
+            throw err;
+        }
+        if (!user) {
+            return done(null, false, {
+                message: 'Unknown user.'
+            });
+        }
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) {
+                throw err;
+            }
+            if (isMatch) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: 'Invalid password'
+                });
+            }
+        });
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.getUserById(id, (err, user) => {
+        done(err, user);
+    });
+});
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login',
